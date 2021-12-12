b2fb97c2275f97bcce8f77e7d3dca5472401000d
Evgeniy Popov <mr.demix@yandex.ru>
2021-12-12 22:26:08 +0300

    Day09 - Second puzzle. Very unoptimized solution, but works fine.

--- src/day09/SmokeBasin.java
+++ src/day09/SmokeBasin.java
@@ -4,20 +4,23 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.ListIterator;
 import java.util.Map;
-import java.util.Objects;
+import java.util.Set;
 
 public class SmokeBasin {
 
+  static Map<Coordinates, Integer> coordinatesMap = new HashMap<>();
+
   public static void main(String[] args) throws IOException {
-    BufferedReader reader = new BufferedReader(new FileReader("src\\day09\\Day09.txt"));
+    BufferedReader reader = new BufferedReader(new FileReader("src\\day09\\Day09_test.txt"));
     String temp;
 
-    int[][] map = new int[100][100];
+    int[][] map = new int[5][10];
     int j = 0;
     while ((temp = reader.readLine()) != null) {
       char[] parsedString = temp.toCharArray();
@@ -27,8 +30,7 @@ public class SmokeBasin {
       j++;
     }
 
-    List<Integer> lowPoints = new ArrayList<>();
-    Map<Coordinates, Integer> coordinatesMap = new HashMap<>();
+    List<Coordinates> lowPoints = new ArrayList<>();
 
     for (int i = 0; i < map.length; i++) {
       for (int k = 0; k < map[0].length; k++) {
@@ -54,51 +56,69 @@ public class SmokeBasin {
         }
         Collections.sort(neighbours);
         if (coordinatesMap.get(new Coordinates(k, i)) < neighbours.get(0)) {
-          lowPoints.add(coordinatesMap.get(new Coordinates(k, i)));
+          lowPoints.add((new Coordinates(k, i)));
         }
       }
     }
 
-    int result = 0;
-    for (Integer i : lowPoints) {
-      int var = i;
+    List<Integer> basinSizes = new ArrayList<>();
+
+    int firstPuzzleResult = 0;
+    for (Coordinates i : lowPoints) {
+      int var = coordinatesMap.get(i);
       var++;
-      result += var;
+      firstPuzzleResult += var;
+      basinSizes.add(neighboursList(i).size());
     }
-    System.out.println(result);
+
+    basinSizes.sort(Collections.reverseOrder());
+    int secondPuzzleResult = 1;
+    for (int i = 0; i < 3; i++) {
+      secondPuzzleResult *= basinSizes.get(i);
+    }
+
+    System.out.println(neighboursList(new Coordinates(0, 0)));
+
+    System.out.println(firstPuzzleResult);
+    System.out.println(secondPuzzleResult);
+  }
+
+  static Set<Coordinates> neighboursList(Coordinates lowPoint) {
+    List<Coordinates> neighbours = new ArrayList<>();
+    neighbours.add(lowPoint);
+
+    // Optimize algorithm. For now program iterate through Coordinates, that already calculated.
+    // Should extract intersection between tempSet and neighbours, and iterate through them
+    while (true) {
+      // Set to compare previous result;
+      Set<Coordinates> tempSet = new HashSet<>();
+      tempSet.addAll(new HashSet<>(neighbours));
+
+      ListIterator<Coordinates> iterator = neighbours.listIterator();
+      while (iterator.hasNext()) {
+        Coordinates point = iterator.next();
+        if (coordinatesMap.get(new Coordinates(point.x + 1, point.y)) != null
+            && coordinatesMap.get(new Coordinates(point.x + 1, point.y)) != 9) {
+          iterator.add(new Coordinates(point.x + 1, point.y));
+        }
+        if (coordinatesMap.get(new Coordinates(point.x - 1, point.y)) != null
+            && coordinatesMap.get(new Coordinates(point.x - 1, point.y)) != 9) {
+          iterator.add(new Coordinates(point.x - 1, point.y));
+        }
+        if (coordinatesMap.get(new Coordinates(point.x, point.y + 1)) != null
+            && coordinatesMap.get(new Coordinates(point.x, point.y + 1)) != 9) {
+          iterator.add(new Coordinates(point.x, point.y + 1));
+        }
+        if (coordinatesMap.get(new Coordinates(point.x, point.y - 1)) != null
+            && coordinatesMap.get(new Coordinates(point.x, point.y - 1)) != 9) {
+          iterator.add(new Coordinates(point.x, point.y - 1));
+        }
+      }
+      if (tempSet.containsAll(new HashSet<>(neighbours))) {
+        break;
+      }
+    }
+    return new HashSet<>(neighbours);
   }
 }
 
-class Coordinates {
-
-  int x;
-  int y;
-
-  Coordinates(int x, int y) {
-    this.x = x;
-    this.y = y;
-  }
-
-  @Override
-  public String toString() {
-    return "x: " + x + "; y: " + y + "; ";
-  }
-
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) {
-      return true;
-    }
-    if (o == null || getClass() != o.getClass()) {
-      return false;
-    }
-    Coordinates that = (Coordinates) o;
-    return x == that.x &&
-        y == that.y;
-  }
-
-  @Override
-  public int hashCode() {
-    return Objects.hash(x, y);
-  }
-}
